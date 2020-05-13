e7ef298991e3afcaed4787e66080c5f168550dce
Brandon Fok <fokb@nlpg01.cs.washington.edu>
2020-05-13 12:27:22 -0700

    bridge and outro models

--- AdvancedApproach/Preprocess_Data/new_train.py
+++ AdvancedApproach/Preprocess_Data/new_train.py
@@ -1,7 +1,7 @@
 import json 
 from textgenrnn import textgenrnn
 
-f = open('sections.json')
+f = open('sections_week8.json')
 data = json.load(f)
 batchsize = 8192
 rnn_size = 128
@@ -9,41 +9,41 @@ dim_embeddings = 100
 max_words = 40000
 gen_epochs = 25
 #%%
-print ('Training intro model')
-intro_gen = textgenrnn(name="intro_model")
-intro_gen.train_on_texts(data['Intro'],
-                        new_model=True,
-                        word_level=True,
-                        num_epochs=50,
-                        gen_epochs=gen_epochs,
-                        rnn_size=rnn_size,
-                        dim_embeddings=dim_embeddings,
-                        max_words=max_words,
-                        batch_size=batchsize)
+#print ('Training intro model')
+#intro_gen = textgenrnn(name="intro_model")
+#intro_gen.train_on_texts(data['Intro'],
+#                        new_model=True,
+#                        word_level=True,
+#                        num_epochs=50,
+#                        gen_epochs=gen_epochs,
+#                        rnn_size=rnn_size,
+#                        dim_embeddings=dim_embeddings,
+#                        max_words=max_words,
+ #                       batch_size=batchsize)
 
-print ('Training chorus model')
-chorus_gen = textgenrnn(name="chorus_model")
-chorus_gen.train_on_texts(data['Chorus'],
-                          new_model=True,
-                          word_level=True,
-                          num_epochs=50,
-                          gen_epochs=gen_epochs,
-                          rnn_size=rnn_size,
-                          dim_embeddings=dim_embeddings,
-                          max_words=max_words,
-                          batch_size=batchsize)
+#print ('Training chorus model')
+#chorus_gen = textgenrnn(name="chorus_model")
+#chorus_gen.train_on_texts(data['Chorus'],
+#                          new_model=True,
+#                          word_level=True,
+#                          num_epochs=50,
+#                          gen_epochs=gen_epochs,
+#                          rnn_size=rnn_size,
+#                          dim_embeddings=dim_embeddings,
+#                          max_words=max_words,
+#                          batch_size=batchsize)
 
-print ('Training verse model')
-verse_gen = textgenrnn(name="verse_model")
-verse_gen.train_on_texts(data['Verse'],
-                        new_model=True,
-                        word_level=True,
-                        num_epochs=50,
-                        gen_epochs=gen_epochs,
-                        rnn_size=rnn_size,
-                        dim_embeddings=dim_embeddings,
-                        max_words=max_words,
-                        batch_size=batchsize)
+#print ('Training verse model')
+#verse_gen = textgenrnn(name="verse_model")
+#verse_gen.train_on_texts(data['Verse'],
+#                        new_model=True,
+#                        word_level=True,
+#                        num_epochs=50,
+#                        gen_epochs=gen_epochs,
+#                        rnn_size=rnn_size,
+#                        dim_embeddings=dim_embeddings,
+#                        max_words=max_words,
+#                        batch_size=batchsize)
 
 print ('Training bridge model')
 bridge_gen = textgenrnn(name="bridge_model")
