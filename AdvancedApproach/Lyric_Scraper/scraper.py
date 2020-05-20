ad5c13c8b2193e48254fd95f7c5acfcaa8c4ae04
Admin <nelson.tan7@gmail.com>
2020-05-19 19:11:28 -0700

    added artists for other genres in lyric scraper

--- AdvancedApproach/Lyric_Scraper/scraper.py
+++ AdvancedApproach/Lyric_Scraper/scraper.py
@@ -11,6 +11,24 @@ file.write('')
 file = open("sams_actually_generating_lyrics.txt", 'a')
 
 artists = ['Lizzo', 'Katy Perry', 'Imagine Dragons', 'Lady Gaga', 'Maroon 5', 'Camila Cabello', 'Sam Smith', '5 Seconds of Summer', 'Bruno Mars', 'Drake']
+artists_country = ['Luke Combs', 'Dan + Shay', 'Kane Brown', 
+                   'Thomas Rhett', 'Florida Georgia Line',
+                   'Chris Stapleton', 'Morgan Wallen',
+                   'Maren Morris', 'Jason Aldean', 'Blake Shelton',
+                   'Luke Bryan', 'Brett Young', 'Carrie Underwood',
+                   'Jon Pardi', 'Zac Brown Band', 'George Strait',
+                   'Old Dominion', 'Eric Church', 'Lee Brice',
+                   'Kacey Musgraves']
+artists_hiphop = ['Khalid', 'Drake', 'Lil Nas X', 'Travis Scott',
+                  'Juice WRLD', 'DaBaby', 'Cardi B', 'Lil Baby',
+                  'Meek Mill', 'A Boogie Wit da Hoodie',
+                  'Lizzo', '21 Savage', 'XXXTENTACION',
+                  'Chris Brown', 'Kodak Black', 'Gunna',
+                  'J. Cole', 'Young Thug', 'Lil Tecca',
+                  'YoungBoy Never Broke Again']
+artists_minecraft = ['MineCraft Awesome Parodys', 'ReptileLegit',
+                     'CaptainSparklez', 'TryHardNinja',
+                     'BebopVox', 'Brad Knauber']
 for artist_name in artists:
 	artist = genius.search_artist(artist_name, max_songs=20)
 	for song in artist.songs:
