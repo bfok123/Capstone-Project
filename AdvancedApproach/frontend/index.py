93db51ccc84e264e837589c5f7018b2c0bdc1839
Admin <nelson.tan7@gmail.com>
2020-05-21 20:01:04 -0700

    frontend variables ready to go

--- AdvancedApproach/frontend/index.py
+++ AdvancedApproach/frontend/index.py
@@ -18,39 +18,57 @@ def onCheck(section):
         bridge_rhyme.configure(state=(tk.DISABLED if var_bridge.get() == 0 else tk.NORMAL))
     if section == 'outro':
         outro_rhyme.configure(state=(tk.DISABLED if var_outro.get() == 0 else tk.NORMAL))
+        
+def generate():
+    model = var.get()
+    topic = entry_topic.get()
+    text_gen.configure(state=tk.NORMAL)
+    text_gen.delete(1.0, tk.END)
+    for key in section_vars:
+        section_var = section_vars[key]
+        if section_var.get() == 1:
+            # get lyrics with model and key
+            rhyming_pattern = section_rhymes[key].get()
+            text_gen.insert(tk.END, '[' + key.capitalize() + ']\n')
+            text_gen.insert(tk.END, rhyming_pattern + '\n\n')
+    text_gen.configure(state=tk.DISABLED)
+        
 
 window = tk.Tk()
 window.title("Lyric Generator")
 pad = 5
 
 genre_frame = tk.Frame()
-var = tk.IntVar() # radio button var
+var = tk.StringVar() # radio button var
+var.set('pop')
 lbl_genre = tk.Label(genre_frame, text="Genre: ")
-r_btn_pop = tk.Radiobutton(genre_frame, text="Pop", variable=var, value=0)
-r_btn_hiphop = tk.Radiobutton(genre_frame, text="Hip Hop", variable=var, value=1)
-r_btn_country = tk.Radiobutton(genre_frame, text="Country", variable=var, value=2)
-r_btn_minecraft = tk.Radiobutton(genre_frame, text="Minecraft", variable=var, value=3)
+r_btn_pop = tk.Radiobutton(genre_frame, text="Pop", variable=var, value='pop')
+r_btn_hiphop = tk.Radiobutton(genre_frame, text="Hip Hop", variable=var, value='hiphop')
+r_btn_country = tk.Radiobutton(genre_frame, text="Country", variable=var, value='country')
+r_btn_minecraft = tk.Radiobutton(genre_frame, text="Minecraft", variable=var, value='minecraft')
 
 sections_frame = tk.Frame()
-var_intro = tk.IntVar()
+var_intro = tk.IntVar() # 1 means selected
 var_verse = tk.IntVar()
 var_chorus = tk.IntVar()
 var_bridge = tk.IntVar()
 var_outro = tk.IntVar()
+section_vars = {'intro': var_intro, 'verse': var_verse, 'chorus': var_chorus, 'bridge': var_bridge, 'outro': var_outro}
 lbl_sections = tk.Label(window, text="Sections to generate and rhyming scheme: ")
 intro_rhyme = tk.Entry(sections_frame, state=tk.DISABLED)
 verse_rhyme = tk.Entry(sections_frame, state=tk.DISABLED)
 chorus_rhyme = tk.Entry(sections_frame, state=tk.DISABLED)
 bridge_rhyme = tk.Entry(sections_frame, state=tk.DISABLED)
 outro_rhyme = tk.Entry(sections_frame, state=tk.DISABLED)
-intro_section = tk.Checkbutton(sections_frame, text="Intro", variable=var_intro, command= lambda: onCheck('intro'))
+section_rhymes = {'intro': intro_rhyme, 'verse': verse_rhyme, 'chorus': chorus_rhyme, 'bridge': bridge_rhyme, 'outro': outro_rhyme}
+intro_section = tk.Checkbutton(sections_frame, text="Intro", variable=var_intro, command=lambda: onCheck('intro'))
 verse_section = tk.Checkbutton(sections_frame, text="Verse", variable=var_verse, command=lambda: onCheck('verse'))
 chorus_section = tk.Checkbutton(sections_frame, text="Chorus", variable=var_chorus, command=lambda: onCheck('chorus'))
 bridge_section = tk.Checkbutton(sections_frame, text="Bridge", variable=var_bridge, command=lambda: onCheck('bridge'))
 outro_section = tk.Checkbutton(sections_frame, text="Outro", variable=var_outro, command=lambda: onCheck('outro'))
 lbl_ex = tk.Label(sections_frame, text="ex: AABB")
 
-btn_gen = tk.Button(window, text="Generate")
+btn_gen = tk.Button(window, text="Generate", command=generate)
 
 topic_frame = tk.Frame()
 lbl_topic = tk.Label(topic_frame, text="Topic: ")
@@ -60,7 +78,6 @@ frame = tk.Frame() # generated text textbox and scrollbar
 text_gen = tk.Text(frame, width=50, state=tk.DISABLED)
 scrollbar = tk.Scrollbar(frame, command=text_gen.yview)
 text_gen['yscrollcommand'] = scrollbar.set
-text_gen.insert(tk.END, 'yo')
 
 # GRID LAYOUT
 genre_frame.grid(column=0, row=0, padx=pad, pady=pad, sticky=tk.W)
